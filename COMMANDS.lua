e3b5d732cdba021a88792951449ee7908c874b48
matterrs <106132062+matterrs@users.noreply.github.com>
2022-06-06 06:16:09 -0700

    Add files via upload

---
+++ COMMANDS.lua
@@ -0,0 +1,36 @@
+-~-~-~- ( Stand Function ) -~-~-~-
+!summon -- Summons stand
+/e summon -- Summons stand (Silent)
+!rejoin -- Makes the stand rejoin
+!commands -- Chats all the commands
+!leave -- Makes stand leave game
+test -- Tests command to see if executed right
+!reset -- Resets stand (Use if stand is buggy)
+-~-~-~- ( Misc Commands ) -~-~-~-
+!drop -- Starts dropping cash
+!wallet -- Equips stand wallet
+!save -- Grabs owner if knocked
+!ghost -- Makes stand invisible
+/e ghost -- Makes stand invisible (Silent)
+!unghost -- Disables ghost command
+!freeze -- Freeze stand in place
+!tp bank -- Brings owner to bank
+!tp casino -- Brings owner to casino
+!tp guns -- Brings owner to guns
+!tp admin -- Brings owner to AB
+!tp uphill -- Brings owner to uphill
+!tp downhill -- Brings owner to downhill
+!tp school -- Brings owner to school
+-~-~-~- ( Kill / Target Commands ) -~-~-~-
+!mimic -- Mimics owners punch / blocks
+!aura --  Kills anyone nearby
+!laura -- Kills anyone nearby (Light punch)
+!sneak -- Hides under owner and rapid punch
+!barrage -- Rapid punch infront of owner
+!multi -- Teleports around owner and punch
+!knock -- Auto knocks target
+!kill -- Auto kills target and stomps
+!trap -- Brings target to sewer
+!jail -- Brings target to jail
+!bring -- Brings target to owner
+Disable commands by using !summon again
